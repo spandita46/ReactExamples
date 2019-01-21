b74fd29ee40798ddcd8130108100024e2076eac4
Sandeep Pandita <sandeeppandita46@gmail.com>
2019-01-21 18:06:08 +0530

    Added Redux DEVTOO Extension Support

--- my-react-seed/src/index.js
+++ my-react-seed/src/index.js
@@ -1,13 +1,17 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import { Provider } from "react-redux";
-import { createStore } from "redux";
+import { createStore, applyMiddleware, compose } from "redux";
 
 import App from "./components/app/App";
 import Reducers from "./reducers";
 
+// Redux Dev Tool Support && Middleware Support
+const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
+const store = createStore(Reducers, composeEnhancers(applyMiddleware()));
+
 ReactDOM.render(
-  <Provider store={createStore(Reducers)}>
+  <Provider store={store}>
     <App />
   </Provider>,
   document.getElementById("root")
