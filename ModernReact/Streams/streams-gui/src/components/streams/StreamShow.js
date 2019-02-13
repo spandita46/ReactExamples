d8f5ac11b0ff1494e671ef6d88909ffc2c2e013a
Sandeep Pandita <sandeeppandita46@gmail.com>
2019-02-13 19:00:55 +0530

    Streaming Enabled

--- ModernReact/Streams/streams-gui/src/components/streams/StreamShow.js
+++ ModernReact/Streams/streams-gui/src/components/streams/StreamShow.js
@@ -1,17 +1,48 @@
 import React from "react";
 import { connect } from "react-redux";
 import { fetchStream } from "../../actions/index";
+import flv from "flv.js";
 
 class StreamShow extends React.Component {
-  componentDidMount() {
-    this.props.fetchStream(this.props.match.params.id);
+  constructor(props) {
+    super(props);
+    this.videoRef = React.createRef();
   }
+  componentDidMount() {
+    const { id } = this.props.match.params;
+    this.props.fetchStream(id);
+    this.buildPlayer();
+  }
+
+  componentDidUpdate() {
+    this.buildPlayer();
+  }
+
+  componentWillUnmount() {
+    this.player.destroy();
+  }
+
+  buildPlayer = () => {
+    if (this.player || !this.props.stream) {
+      return;
+    }
+    const { id } = this.props.match.params;
+    // FLV Video Player Settings
+    this.player = flv.createPlayer({
+      type: "flv",
+      url: `http://localhost:8000/live/${id}.flv`
+    });
+    this.player.attachMediaElement(this.videoRef.current);
+    this.player.load();
+  };
+
   render() {
     if (!this.props.stream) {
       return <div>Loading...</div>;
     }
     return (
       <div>
+        <video ref={this.videoRef} style={{ width: "100%" }} controls />
         <h1>{this.props.stream.title}</h1>
         <h5>{this.props.stream.description}</h5>
       </div>
