8fd9a8d5698a89353474971e8c408a30fb9d935a
Sandeep Pandita <sandeeppandita46@gmail.com>
2019-02-15 14:44:18 +0530

    Hook Resuse

--- ModernReact/seasonWithHook/src/components/app/App.js
+++ ModernReact/seasonWithHook/src/components/app/App.js
@@ -1,17 +1,10 @@
-import React, { Component, useState, useEffect } from "react";
+import React from "react";
 import DisplaySeason from "../displaySeason/displaySeason";
 import Loader from "../loader/loader";
+import useLocation from "../../helper/useLocation";
 
 const App = () => {
-  const [lat, setLat] = useState(null);
-  const [errorMessage, setErrorMessage] = useState("");
-
-  useEffect(() => {
-    window.navigator.geolocation.getCurrentPosition(
-      position => setLat(position.coords.latitude),
-      error => setErrorMessage(error.message)
-    );
-  }, []);
+  const [lat, errorMessage] = useLocation();
 
   let content;
   if (errorMessage) {
@@ -26,7 +19,7 @@ const App = () => {
     content = <Loader size="massive" text="Detecting Location & Weather" />;
   }
 
-  return <div className="boreder red">{content}</div>;
+  return <div className="border red">{content}</div>;
 };
 
 export default App;
